6ed3d3adaf6cda7e1ce6083a252c5373a55e0ead
Clarice Tran <tran.clarice@gmail.com>
2023-01-05 01:43:03 -0800

    updated highscores.js to print high scores from local storage

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # quiz-application
 
 [Tutorial for sorting an array of objects](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#sorting_array_of_objects)
+[Tutorial for redirecting to another page in Javascript](https://www.w3schools.com/howto/howto_js_redirect_webpage.asp)
--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,1 +1,15 @@
 // This script will grab the localStorage data of the current leaderboard and display it based of off the highest to lowest scores to display on the highscores page.
+var leaderboard = document.querySelector("tbody");
+
+var scores = JSON.parse(localStorage.getItem("scores"));
+
+scores.forEach((data) => {
+	var listScore = document.createElement("tr");
+	var name = document.createElement("td");
+	name.textContent = data.name;
+	listScore.append(name);
+	var score = document.createElement("td");
+	score.textContent = data.score;
+	listScore.append(score);
+	leaderboard.append(listScore);
+});
